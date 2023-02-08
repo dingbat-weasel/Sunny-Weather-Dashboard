449128db72be83f00f104210d1303cb73ca7d899
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-02-07 19:59:08 -0800

    get coord and get weather fetch functions running

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,41 @@ const iconEl = document.querySelector(".icon");
 const temperatureEl = document.querySelector(".temperature");
 const windSpeedEl = document.querySelector(".windSpeed");
 
-let lat;
-let lon;
+const APIKey = "1f2ae3b57cb7c607e6bdd221bdb7a3b2";
 
-const getCoordinates = function (userInput) {
-  //s9tfgSkphL1BZu8odgY8Urm2XijdrfYbEPglLjZ9
-  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=9tfgSkphL1BZu8odgY8Urm2XijdrfYbEPglLjZ9`;
-
-  fetch(requestUrl).then(function (response) {
-    console.log(response.status);
-    console.log(response);
-    lat = response.lat;
-    lon = response.lon;
-    return response.json();
-  });
+const userCity = {
+  cityName: "",
+  latitude: "",
+  longitude: "",
+  weather: "",
 };
 
-const checkWeather = function (lat, lon) {};
-
+// Generate Weather Data from User Input on Search Click
 searchBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  getCoordinates(document.querySelector("#cityGet").value);
-  console.log(lat, lon);
+  // Get Coordinates
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${
+      document.querySelector("#cityGet").value
+    }&limit=1&appid=${APIKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      userCity.cityName = data[0].name;
+      userCity.latitude = data[0].lat;
+      userCity.longitude = data[0].lon;
+      console.log(userCity);
+
+      // Get Weather from Coordinates
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          userCity.weather = data.weather[0].main;
+          console.log(userCity);
+        });
+    });
 });
